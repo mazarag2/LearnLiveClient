9fda611eda3f6f9c43cf5fbd4f847c1aa919e1f2
mazarag2 <mazarag2@asu.edu>
2018-07-07 12:14:55 -0700

    Added basic Vue integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+
+var response = new Vue({
+	
+	el: '#DisplayResponse',
+	data: {
+		response: 'IsitLikeDarkSouls well see..'
+	}
+})
+
+new Vue({
+	
+	el: '#gameName',	
+	methods:{
+		
+		sendGameName: function(){
+			
+			
+			
+			/*
+			  mounted () {
+				axios
+				  .get('https://api-endpoint.igdb.com/games/')
+				  .then(response => (this.info = response))
+			  }
+			  */
+			//send msg to API
+			
+			
+		}
+	}
+	
+	
+	
 })
