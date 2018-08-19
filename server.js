b9764e2af7c3c711399c6ba999ae2752867a94a6
mazarag2 <mazarag2@asu.edu>
2018-08-19 13:10:21 -0700

    added app to route

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var path = require("path");
 var serveStatic = require("serve-static");
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
+console.log(path.join(__dirname, "dist")));
+app.use(express.static(__dirname));
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log("server started "+port);
+app.get('*',function(req,res){
+	
+	
+	res.sendfile(path.resolve(__dirname,'src/app.vue'));
+	
+	
+});
