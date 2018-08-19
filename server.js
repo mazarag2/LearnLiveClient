1e1126c08edddec00308255c759b42280b596672
mazarag2 <mazarag2@asu.edu>
2018-08-19 13:13:27 -0700

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var serveStatic = require("serve-static");
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-console.log(path.join(__dirname, "dist")));
+console.log(path.join(__dirname, "dist"));
 app.use(express.static(__dirname));
 var port = process.env.PORT || 8080;
 app.listen(port);
